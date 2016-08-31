ec8dc9e33562d83fe885f99fbce8db5037baff6d
Luan Lima <luanyata@gmail.com>
2016-08-30 23:12:56 -0300

    Adição do Toask para mensagem de alerta informando que o campo está em branco; Atualização da plataforma; Adição do Icone;

--- app/src/main/java/com/yata/idadedecachorro/MainActivity.java
+++ app/src/main/java/com/yata/idadedecachorro/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,11 +32,10 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                //Recuperar digitado
                 String textoDigitado = caixaTexto.getText().toString();
 
                 if (textoDigitado.isEmpty()) {
-                    resultado.setText("Nenhum Numero digitado");
+                    Toast.makeText(getApplicationContext(), "Digite a idade do Cachorro", Toast.LENGTH_LONG).show();
                 } else {
                     int valorDigitado = Integer.parseInt(textoDigitado);
                     int resultadoFinal = valorDigitado * 7;
@@ -45,27 +45,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
